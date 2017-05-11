ed8d0e16c35aa991236ad4ad350f2ca41d97a4b1
Daniel Tattan-Birch <dantb@hotmail.co.uk>
2017-05-11 21:06:36 +0100

    Threading experimentation, initial commit

---
+++ DotNetExperimentation/DotNetExperimentation/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace DotNetExperimentation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.Write("Press a key to continue\n");
+                Console.ReadKey();
+                ThreadingStuff threadClass = new ThreadingStuff();
+                threadClass.TryMultithreading();
+            }
+        }
+    }
+}
---
+++ DotNetExperimentation/DotNetExperimentation/ThreadingStuff.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace DotNetExperimentation
+{
+    public class ThreadingStuff
+    {
+        public void TryMultithreading()
+        {
+            SomeClass aClass = new SomeClass();
+
+            Thread thread = new Thread(() => aClass.DoSomething("A"));
+            thread.Start();
+
+            //set task off
+            Task theTask = new Task(() => aClass.DoSomething("B"));
+            theTask.Start();
+            theTask.Wait();
+            //now just call it here
+            aClass.DoSomething("C");
+        }
+
+    }
+
+    public class SomeClass
+    {
+        private int _someMember; 
+
+        public void DoSomething(string thread)
+        {
+            Console.WriteLine($"Thread {thread}, counter value {_someMember}");
+            _someMember++;
+        }
+
+    }
+}
