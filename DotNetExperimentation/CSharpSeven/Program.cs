7a51b22969e481bae3b5e3e1b8560f0fcd1ab8c2
Daniel Tattan-Birch <dantb@hotmail.co.uk>
2017-07-20 23:37:14 +0100

    Project added to test C# 7 language features.
    Tested out embedded local functions and digit separators

---
+++ DotNetExperimentation/CSharpSeven/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpSeven
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int bigNumber = 1_000_000_000;
+
+            WriteBigNumber();
+
+            WriteLine("Press any key to exit...");
+            Console.ReadKey();
+
+            void WriteLine(string message)
+            {
+                Console.WriteLine(message);
+            }
+
+            void WriteBigNumber()
+            {
+                WriteLine(bigNumber.ToString());
+            }
+        }
+    }
+}
