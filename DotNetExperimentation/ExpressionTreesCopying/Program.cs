dd7d5952f7f657bf1e5fc7dcff25f909d4ff7d05
dantb <dantb@hotmail.co.uk>
2017-08-20 20:53:00 +0100

    Adding project to demonstrate issue I found with deep copy by expression trees, found in this article: https://www.codeproject.com/Articles/1111658/Fast-Deep-Copy-by-Expression-Trees-C-Sharp
    I've left a comment on the article and I'm currently waiting for the author to respond - I'd like to be able to trust this form of cloning as I can see its performance benefits, as well as ease of use.

---
+++ DotNetExperimentation/ExpressionTreesCopying/Program.cs
@@ -0,0 +1,55 @@
+﻿using DeepCopyExpressionTreeHashSetIssue;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ExpressionTreesCopying
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            B b = new B();
+            A a = new A(b);
+
+            // result is true
+            bool testEqualBeforeClone =
+                object.ReferenceEquals(a.DependencyOnB.SetOfCs.First(), a.SharedC);
+
+            // result is true
+            bool containsBeforeClone = a.DependencyOnB.SetOfCs.Contains(a.SharedC);
+
+            // make a clone
+            A clonedA = a.DeepCopyByExpressionTree();
+
+            // result is true
+            bool testEqualAfterClone =
+                object.ReferenceEquals(clonedA.DependencyOnB.SetOfCs.First(), clonedA.SharedC);
+
+            // result is false
+            bool contains = clonedA.DependencyOnB.SetOfCs.Contains(clonedA.SharedC);
+
+            Console.ReadKey();
+        }
+
+
+        public class A
+        {
+            public B DependencyOnB;
+            public C SharedC = new C();
+
+            public A(B dep)
+            {
+                DependencyOnB = dep;
+                DependencyOnB.SetOfCs.Add(SharedC);
+            }
+        }
+
+        public class B
+        {
+            public HashSet<C> SetOfCs { get; } = new HashSet<C>();
+        }
+
+        public class C { }
+    }
+}
